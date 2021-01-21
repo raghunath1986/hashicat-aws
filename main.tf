d0600d8b19e09977714b3c68299bb1290e891d55
raghunath1986 <39937557+raghunath1986@users.noreply.github.com>
2021-01-21 15:54:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    aws_instance = "devops"
+    Department = "devops"
     Billable = "true"
   }
 }
